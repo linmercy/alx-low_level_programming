b36e8cfe1b4ebbe866ccc5c0c320b7be82d9dd0b
mercylin <linmercymuthoni@gmail.com>
2023-06-08 10:17:05 -0700

    get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask = 1UL << index;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n |= (1UL << index);
-	return (1);
+	if ((n & mask) != 0)
+		return (1);
+	else
+		return (0);
 }
